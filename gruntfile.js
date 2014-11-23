af1c600f124d392f08a088d79f99908c243b5da3
Albert Besuhoff <albert.besuhoff@gmail.com>
2014-11-23 10:37:01 +0200

    PR-I: Take port number from config

--- gruntfile.js
+++ gruntfile.js
@@ -2,7 +2,8 @@ module.exports = function(grunt) {
   grunt.initConfig({
     server: {
       options: {
-        server: './server.js'
+        server: './server.js',
+        port: Number(process.env.PORT || 8000)
       }
     }
   });
@@ -13,7 +14,7 @@ module.exports = function(grunt) {
   grunt.registerTask('server', 'Start a web server', function() {
     var name = this.name;
     grunt.config.requires([name, 'options', 'server']);
-    var port = Number(process.env.PORT || 8000);
+    var port = Number(grunt.config([name, 'options', 'port']) || 8000);
     var done = this.async();
     grunt.log.writeln('Starting web server on port ' + port + '.');
     require(grunt.config([name, 'options', 'server'])).listen(port).on('close', done);
